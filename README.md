21eb81b881b585cc0ef00c0c1a6132a61a230124
Calvin Wilkinson <kinsondigital@gmail.com>
2022-05-26 08:31:13 +0100

    Remove old TS codebase and project

--- README.md
+++
@@ -1,3 +0,0 @@
-# Nuget Version Checker
-
-Refer to the [docs](./documentation/docs.md) for more information.
--- src/interfaces/PackageVersionResponse.ts
+++
@@ -1,9 +0,0 @@
-/**
- * The package version response from the Nuget API call.
- */
-export interface PackageVersionResponse {
-	/**
-	 * The list of nuget package versions for a package in the nuget.org repository.
-	 */
-	versions: string[];
-}
