01685ddd31b3187ddd61c8bbfd959389cab32d8b
Calvin Wilkinson <kinsondigital@gmail.com>
2023-05-04 15:09:32 +0100

    ci: create status check for version and release note checks

--- .github/cicd/validate-notes.ts
+++ .github/cicd/validate-notes.ts
@@ -3,16 +3,22 @@ import { existsSync } from "https://deno.land/std@0.184.0/fs/exists.ts";
 // Validate the arguments
 if (Deno.args.length !== 2) {
     let errorMsg = "The 'validate-notes' cicd script must have two arguments.";
-    errorMsg += "\nThe first arg must be either 'production' or 'preview'.";
+    errorMsg += "\nThe first arg must be either 'production', 'preview' or 'either'.";
     errorMsg += "\nThe second arg must be the version of the notes.";
 
     throw new Error(errorMsg);
 }
 
 const notesType: string = Deno.args[0].toLowerCase();
+const version: string = Deno.args[1];
 
-if (notesType !== "production" && notesType !== "preview") {
-    let errorMsg = "The notes type argument must be a value of 'production' or 'preview'.";
+console.log("::group::Argument Values");
+console.log(`Notes Type: ${notesType}`);
+console.log(`Version: ${version}`);
+console.log("::endgroup::");
+
+if (notesType !== "production" && notesType !== "preview" && notesType != "either") {
+    let errorMsg = "The notes type argument must be a value of 'production', 'preview' or 'either'.";
     errorMsg += "\nThe value is case-insensitive.";
 
     throw new Error(errorMsg);
@@ -21,7 +27,6 @@ if (notesType !== "production" && notesType !== "preview") {
 const prodVersionRegex = /^v[0-9]+\.[0-9]+\.[0-9]+$/;
 const prevVersionRegex = /^v[0-9]+\.[0-9]+\.[0-9]+-preview\.[0-9]+$/;
 
-const version: string = Deno.args[1];
 let isValid = false;
 
 if (notesType === "production") {
@@ -34,7 +39,15 @@ if (isValid === false) {
     throw new Error(`The version is not in the correct ${notesType} version syntax.`);
 }
 
-const notesDirName = notesType === "production" ? "ProductionReleases" : "PreviewReleases";
+let notesDirName = "";
+
+if (notesType == "production" || notesType === "preview") {
+    notesDirName = notesType === "production" ? "ProductionReleases" : "PreviewReleases";
+} else {
+    const isPrevTag: boolean = version.indexOf("-preview.") != -1;
+    notesDirName = isPrevTag ? "PreviewReleases" : "ProductionReleases";
+}
+
 const notesFilePath = `${Deno.cwd()}/Documentation/ReleaseNotes/${notesDirName}/Release-Notes-${version}.md`;
 
 if (!existsSync(notesFilePath)) {
--- .github/cicd/validate-tag.ts
+++ .github/cicd/validate-tag.ts
@@ -1,44 +1,56 @@
 // Validate the arguments
 if (Deno.args.length !== 3) {
     let errorMsg = "The 'validate-tag' cicd script must have two arguments.";
-    errorMsg += "\nThe first arg must be either 'production' or 'preview'.";
+    errorMsg += "\nThe first arg must be either 'production', 'preview' or 'either'.";
     errorMsg += "\nThe second arg must be the name of the tag.";
 
     throw new Error(errorMsg);
 }
 
 const tagType: string = Deno.args[0].toLowerCase();
+const tag: string = Deno.args[1].startsWith("v") ? Deno.args[1] : `v${Deno.args[1]}`;
+const projectName: string = Deno.args[2];
 
-if (tagType !== "production" && tagType !== "preview") {
-    let errorMsg = "The tag type argument must be a value of 'production' or 'preview'.";
+console.log("::group::Argument Values")
+console.log(`Tag Type: ${tagType}`);
+console.log(`Tag: ${tag}`);
+console.log(`Project Name: ${projectName}`);
+console.log("::endgroup::");
+
+if (tagType !== "production" && tagType !== "preview" && tagType !== "either") {
+    let errorMsg = "The tag type argument must be a value of 'production', 'preview' or 'either'.";
     errorMsg += "\nThe value is case-insensitive.";
 
     throw new Error(errorMsg);
 }
 
-const projectName: string = Deno.args[2];
-
 const prodVersionRegex = /^v[0-9]+\.[0-9]+\.[0-9]+$/;
 const prevVersionRegex = /^v[0-9]+\.[0-9]+\.[0-9]+-preview\.[0-9]+$/;
 
-let tag: string = Deno.args[1];
-
-tag = tag.startsWith("v")
-    ? tag
-    : `v${tag}`;
-
 let isValid = false;
 
-if (tagType === "production") {
-    isValid = prodVersionRegex.test(tag);
-} else {
-    isValid = prevVersionRegex.test(tag);
+switch (tagType) {
+    case "production":
+        isValid = prodVersionRegex.test(tag);
+        break;
+        case "preview":
+        isValid = prevVersionRegex.test(tag);
+        break;
+    case "either":
+        isValid = prodVersionRegex.test(tag) || prevVersionRegex.test(tag);
+        break;
+    default:
+        break;
 }
-
+        
 if (isValid === false) {
-    throw new Error(`The tag is not in the correct ${tagType} version syntax.`);
+    const tagTypeStr = tagType === "production" || tagType === "preview"
+    ? tagType
+    : "production or preview";
+    
+    throw new Error(`The tag is not in the correct ${tagTypeStr} version syntax.`);
 }
-
+        
 const tagsUrl = `https://api.github.com/repos/KinsonDigital/${projectName}/tags`;
 const response = await fetch(tagsUrl);
 const responseData = <{ name: ""}[]>await response.json();
