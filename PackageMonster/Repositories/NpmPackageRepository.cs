b7059bad60c3bd5a9eac88ec397b98a29420030f
Charles Mathis <chmathis@factset.com>
2023-05-25 12:10:40 -0400

    fixed npm json path

--- PackageMonster/Repositories/NpmPackageRepository.cs
+++ PackageMonster/Repositories/NpmPackageRepository.cs
@@ -3,5 +3,5 @@
 internal class NpmPackageRepository : IPackageRepository
 {
     public string Url => "https://registry.npmjs.org/PACKAGE-NAME";
-    public string JsonPath => "$.versions[*].version";
+    public string JsonPath => "$.versions.*.version";
 }
--- Testing/PackageMonsterTests/Models/PackageVersionsModelTests.cs
+++ Testing/PackageMonsterTests/Models/PackageVersionsModelTests.cs
@@ -19,11 +19,50 @@ public class PackageVersionsModelTests
 {
     #region Prop Tests
     [Fact]
-    public void Version_WhenSettingValue_ReturnsCorrectResult()
+    public void Version_WhenUsingNugetJsonPath_ReturnsCorrectResult()
     {
         // Arrange
         var packageRepository = new NugetPackageRepository();
-        var model = JObject.Parse(@"{ 'versions': [""1.2.3"", ""4.5.6""] }");
+        var model = JObject.Parse(@"
+{ 
+    ""versions"": [
+        ""1.2.3"", 
+        ""4.5.6""
+    ]
+}
+");
+
+        // Act
+        var actual = model.SelectTokens(packageRepository.JsonPath).Select(v => v.Value<string>()).ToArray();
+
+        // Assert
+        actual.Should()
+            .HaveCount(2)
+            .And.Contain("1.2.3")
+            .And.Contain("4.5.6")
+            .And.HaveElementPreceding("4.5.6", "1.2.3");
+    }
+    #endregion
+
+
+    #region Prop Tests
+    [Fact]
+    public void Version_WhenUsingNpmJsonPath_ReturnsCorrectResult()
+    {
+        // Arrange
+        var packageRepository = new NpmPackageRepository();
+        var model = JObject.Parse(@"
+{
+  ""versions"": {
+    ""1.2.3"": {
+      ""version"": ""1.2.3""
+    },
+    ""4.5.6"": {
+      ""version"": ""4.5.6""
+    }
+  }
+}
+");
 
         // Act
         var actual = model.SelectTokens(packageRepository.JsonPath).Select(v => v.Value<string>()).ToArray();
